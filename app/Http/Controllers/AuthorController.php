3d1d15fd3307302a860bc49a02d220eb0a084e1f
Laura Camila Páez Barbosa <59691866+LauraCamilaPaez@users.noreply.github.com>
2021-02-05 22:59:25 -0500

    Update API

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use http\Env\Response;
+use App\Models\Author;
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class AuthorController extends Controller
 {
+
+    Use ApiResponser;
+
     /**
      * Create a new controller instance.
      *
@@ -23,7 +30,8 @@ class AuthorController extends Controller
      * @return Illuminate\Http\Response;
      */
     public function index(){
-
+        $authors = Author::all();
+        return $this->successResponse($authors);
     }
 
     /**
@@ -32,7 +40,17 @@ class AuthorController extends Controller
      * @return Illuminate\Http\Response;
      */
     public function store(Request $request){
+        $rules =[
+            'name' => 'required|max:255',
+            'gender' => 'required|max:255|in:male,female',
+            'country' => 'required|max:255',
+        ];
 
+        $this->validate($request, $rules);
+
+        $authors = Author::create($request->all());
+
+        return $this->successResponse($authors, Response::HTTP_CREATED);
     }
 
     /**
@@ -41,6 +59,10 @@ class AuthorController extends Controller
      * @return Illuminate\Http\Response;
      */
     public function show($id){
+
+        $authors = Author::findOrFail($id);
+
+        return $this->successResponse($authors);
 
     }
 
@@ -51,7 +73,24 @@ class AuthorController extends Controller
      * @return Illuminate\Http\Response;
      */
     public function update(Request $request, $id){
+        $rules =[
+            'name' => 'max:255',
+            'gender' => 'max:255|in:male,female',
+            'country' => 'max:255',
+        ];
+        $this->validate($request, $rules);
 
+        $author = Author::findOrFail($id);
+
+        $author->fill($request->all());
+
+        if ($author->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $author->save();
+
+        return  $this->successResponse($author);
     }
 
     /**
---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Response;
+
+trait ApiResponser{
+
+    /*
+     * Build a success response
+     * @param String | Array $data
+     * @param int $code
+     * @return Illuminate\Http\JsonResponse
+     * */
+    public function successResponse($data, $code = Response::HTTP_OK){
+        return response()->json(['data'=> $data], $code);
+    }
+
+    /*
+    * Build an error response
+    * @param String $message
+    * @param int $code
+    * @return Illuminate\Http\JsonResponse
+
+    * */
+    public function errorResponse(){
+        return response()->json(['error' => $message, 'code' => $code], $code);
+
+    }
+}
